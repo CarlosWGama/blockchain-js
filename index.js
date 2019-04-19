50fec47549f6d56bfd0ad397e551e92acddde8cc
Carlos W. Gama <carloswgama@gmail.com>
2019-04-19 14:58:25 -0300

    Rodando numa API

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+//API
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//Blockchain
 let Bloco = require('./classes/bloco');
 let Blockchain = require('./classes/blockchain');
 
 let bc = new Blockchain();
-//Cria o Bloco
-let bloco = new Bloco(bc.tamanho, bc.ultimoHash(), bc.dificuldade);
-bloco.adicionarInformacao('Carlos', 'Hello World');
-bloco.adicionarInformacao('Carlos', 'Certificado X na data dd/mm/YYYY');
-bloco.criarHash();
 
-//Adiciona a Blockchain
-bc.adicionarBloco(bloco);
+//-------------- ROTAS ----------------//
+//Retornar os blocos da Blockchain
+app.get('/blockchain', (req, res) => {
+    res.send(bc.blocos);
+});
 
-console.log(bc.blocos);
+//Salva nova informação na Blockchain
+app.post('/blockchain', (req, res) => {
+    let bloco = new Bloco(bc.tamanho, bc.ultimoHash(), bc.dificuldade);
+
+    //Recebe um objeto do tipo {autor:string, msg:string}[]
+    req.body.forEach((dado) => {
+        bloco.adicionarInformacao(dado.autor, dado.msg);
+    })
+    
+    bloco.criarHash();
+
+    bc.adicionarBloco(bloco);
+    res.send(bloco);
+});
+
+app.listen('3000', () => {
+    console.log('Servidor Inicido na porta 3000');
+});
